f98f4902c2f614b21c337e627f115806028dc3d9
Allyson Xavier <allysonxavier3@gmail.com>
2022-08-25 11:21:55 -0300

    creating functions

--- main.js
+++ main.js
@@ -1,10 +1,43 @@
 const KEY_LOCAL_TASKS = "TasksToDo";
 
-const valueInput = document.getElementById("inputValue").value;
-const addValue = document.getElementById("add");
-const list = document.getElementsById("listValue");
-addValue.addEventListener("click", function saveValue(elementLi) {
-  addValue.createElement("li");
-  document.querySelector("list").appendChild(addValue);
-  list.innerHTML = '<li><img src="./css/img/trash-solid.svg" /> </li>';
-});
+const inputButton = document.getElementById("inputButton");
+inputButton.addEventListener("click", saveValue);
+const listValue = document.getElementById("listValue");
+
+const inputText = document.createElement("span");
+const inputDelete = document.createElement("img");
+
+// const inputButton = document.getElementById("inputButton");
+
+function insertItem(text) {
+  const listItem = document.createElement("li");
+  listItem.innerHTML = text;
+  listValue.appendChild(listItem);
+}
+
+function managerInput() {
+  const inputValue = document.getElementById("inputValue");
+  const value = inputValue.value;
+  inputValue.value = "";
+  return value;
+}
+
+insertItem("teste");
+
+function saveValue() {
+  const text = getValueInput();
+  clearInput();
+  insertItem(text);
+  uptadeStorage();
+
+  // const text = managerInput();
+  // console.log("saveValue called with", text);
+
+  // insertItem(new Date().toString());
+
+  // listItem.appendChild(inputButton);
+  // listItem.appendChild(inputText);
+}
+
+// document.querySelector("#listValue").innerHTML =
+//   "<li class='tasksToDo' id='tasksToDo'> <span class='list__text'>Lorem ipsum</span> <img class='delete__img' src='./css/img/trash-solid.svg' /></li>";
